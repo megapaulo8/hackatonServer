a05868490db7a3d846f26ca1ab83da6ac9f958f5
Paulo <paulosantos194@hotmail.com>
2019-03-09 13:23:37 -0300

    Initial Commit

---
+++ src/main/java/com/hackaton/windev/WindevApplication.java
@@ -0,0 +1,13 @@
+package com.hackaton.windev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WindevApplication.class, args);
+	}
+
+}
